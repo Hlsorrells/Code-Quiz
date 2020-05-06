c46f3d83faf37d7855653cc5c8ca798cbf632084
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-06 09:50:56 -0400

    created the question game play including making answer buttons visible during play

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 // Create all necessary global variables for HTML elements
+var carousel = document.querySelector(".carouselbox");
 var imageEl = document.querySelector(".card-img-top");
 var cardTitleEl = document.querySelector(".card-title");
 var cardTextEl = document.querySelector(".card-text");
 var buttonArrayEl = document.querySelector(".btn-group-vertical");
+var button1El = document.querySelector("#button1");
+var button2El = document.querySelector("#button2");
+var button3El = document.querySelector("#button3");
+var button4El = document.querySelector("#button4");
 var startBtn = document.querySelector("#start");
+var timerEL = document.querySelector("#timer");
 
 // Incremental counter index variables
-var count = 60;
-var questionNumber = 0;
+var secondsLeft = 60;
+var questionIndex = 0;
+var currentScore = 0;
+var score = localStorage.getItem("score");
 
 // Array variable to contain the question objects
 var questionArray = [
@@ -41,38 +49,56 @@ var questionArray = [
         correct: "b. Idaho",
         image: "http://placekitten.com/200/200"
     },
-
 ];
 
+// Timer function set to 1 second interval
+// function startTimer() {
+//     div.appendChild(timerEL);
+//     var timerInterval = setInterval(function () {
+//         secondsLeft--;
+//         timerEl.textContent = secondsLeft + " seconds left!";
 
-// Timer count down function
-// function startTimer()
+//         if (secondsLeft === 0) {
+//             clearInterval(timerInterval);
+//             // sends to high score
+//             endGame();
+//         }
+//     }, 1000);
+// }
 
-// Welcome Card
+// End game function
+function endGame() {
+    console.log(currentScore);
+    currentScore.textContent = score;
+    localStorage.setItem("score", score);
+}
+
+// Game Play function
 function game() {
 
     // Hide the start button
     document.querySelector("#start").style.display = "none";
 
-    // Create button variables
-    var button1El = document.createElement("button");
-    var button2El = document.createElement("button");
-    var button3El = document.createElement("button");
-    var button4El = document.createElement("button");
+    // Add style to variables
+    button1El.setAttribute("class", "btn btn-primary btn-sm d-block");
+    button2El.setAttribute("class", "btn btn-primary btn-sm d-block");
+    button3El.setAttribute("class", "btn btn-primary btn-sm d-block");
+    button4El.setAttribute("class", "btn btn-primary btn-sm d-block");
 
     // Clear variable content
     imageEl.innerHTML = "";
     cardTitleEl.innerHTML = "";
     cardTextEl.innerHTML = "";
 
+    console.log(questionIndex);
     // Add content to variables
-    imageEl.setAttribute("src", questionArray[questionNumber].image);
-    cardTitleEl.innerHTML = "Question #" + (questionNumber + 1);
-    cardTextEl.textContent = questionArray[questionNumber].question;
-    button1El.textContent = questionArray[questionNumber].answers[0];
-    button2El.textContent = questionArray[questionNumber].answers[1];
-    button3El.textContent = questionArray[questionNumber].answers[2];
-    button4El.textContent = questionArray[questionNumber].answers[3];
+    imageEl.setAttribute("src", questionArray[questionIndex].image);
+    cardTitleEl.innerHTML = "Question #" + (questionIndex + 1);
+    cardTextEl.textContent = questionArray[questionIndex].question;
+    button1El.textContent = questionArray[questionIndex].answers[0];
+    button2El.textContent = questionArray[questionIndex].answers[1];
+    button3El.textContent = questionArray[questionIndex].answers[2];
+    button4El.textContent = questionArray[questionIndex].answers[3];
 
     // Append variable content to DOM elements
     buttonArrayEl.appendChild(button1El);
@@ -80,36 +106,41 @@ function game() {
     buttonArrayEl.appendChild(button3El);
     buttonArrayEl.appendChild(button4El);
 
-    // Add style to variables
-    button1El.setAttribute("type", "button");
-    button1El.setAttribute("class", "btn btn-primary btn-sm");
-    button1El.setAttribute("style", "margin: 5px auto");
-    button2El.setAttribute("type", "button");
-    button2El.setAttribute("class", "btn btn-primary btn-sm");
-    button2El.setAttribute("style", "margin: 5px auto");
-    button3El.setAttribute("type", "button");
-    button3El.setAttribute("class", "btn btn-primary btn-sm");
-    button3El.setAttribute("style", "margin: 5px auto");
-    button4El.setAttribute("type", "button");
-    button4El.setAttribute("class", "btn btn-primary btn-sm");
-    button4El.setAttribute("style", "margin: 5px auto");
-
     // Grade whether the user response is true
-    // function displayGrade() {
-    //     if (questionArray.correct[questionNumber]) {
-    //         prompt("Correct");
-    //     } else {
-    //         prompt("Wrong answer!");
-    //         //adjust timer by -10 seconds
-    //     }
-    // };
-
+    function displayGrade(event) {
+        console.log(questionIndex);
+        var correctAnswer = questionArray[questionIndex].correct;
+        console.log(correctAnswer);
+        if (event.target.textContent === correctAnswer) {
+            alert("Correct!! You just got 10 points!!!");
+            // Adjust current score by 10 points
+            currentScore += 10;
+            console.log(currentScore);
+            navigate();
+        } else {
+            alert("Wrong answer!");
+            // Adjust timer by -10 seconds
+            secondsLeft -= 10;
+            navigate();
+        }
+    };
     // Listen for answer button click
-    // button1El.addEventListener("click", displayGrade);
+    button1El.addEventListener("click", displayGrade);
+    button2El.addEventListener("click", displayGrade);
+    button3El.addEventListener("click", displayGrade);
+    button4El.addEventListener("click", displayGrade);
+}
 
-    // Advance to next question by increasing the questionNumber value
-    questionNumber++;
-
+// Carousel function
+function navigate() {
+    // Advance to next question by increasing the questionIndex value
+    questionIndex++;
+    // Verify condition of next question and 
+    if (questionIndex < questionArray.length && secondsLeft != 0) {
+        game();
+    } else {
+        endGame();
+    }
 }
 
 // Start button listeners
@@ -117,7 +148,9 @@ startBtn.addEventListener("click", game);
 // startBtn.addEventListener("click", startTimer);
 
 
-// Adjust the score
-function (adjustScore) {
 
-}
+
+// User input of initials
+// input(Please enter your initials)
+
+// button to leaderboard
