b65f5000b893f59cb2ceb66bb73f9904a59d3068
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-11 23:43:20 -0400

    added clear button listener to clear local storage and remove body from table

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,19 +1,44 @@
+var tableEl = document.querySelector(".table");
+var clearBtn = document.querySelector("#clear");
+
 // Pull recent score stats from local storage
 function updateBoard() {
     // Retrieve local storage values
     var score = localStorage.getItem("score");
     var user = localStorage.getItem("user");
 
-    // Insert a new row into the Leader Table
-    var table = document.querySelector("#table-body");
-    var row = document.querySelector("#leaderRow");
+    // Create table body under table header
+    var tbodyEl = document.createElement("tbody");
+    tableEl.appendChild(tbodyEl);
+    var trowEl = document.createElement("tr");
+    tbodyEl.appendChild(trowEl);
+
+    // Insert last game results
     var firstCell = document.createElement("td");
-    row.appendChild(firstCell);    
-    var secondCell = document.createElement("td");
-    row.appendChild(secondCell);
+    trowEl.appendChild(firstCell);
     firstCell.textContent = user;
+    var secondCell = document.createElement("td");
+    trowEl.appendChild(secondCell);
     secondCell.textContent = score;
 
 }
 
+// Update the Leader Board Results
 updateBoard();
+
+// Clear the body from the table
+function resetBoard() {
+    // Reset local storage values
+    localStorage.setItem("score", "");
+    localStorage.setItem("user", "");
+
+    // Remove all rows from table body
+    var tbodyEl = document.querySelector("tbody");
+    tbodyEl.remove();
+
+    // Recall updateBoard function to clear cells
+    updateBoard();
+}
+
+// Clear button listener
+clearBtn.addEventListener("click", resetBoard);
