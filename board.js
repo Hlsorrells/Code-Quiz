6b9b419c578a3b21e744e69b92552b345861758b
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-11 22:38:52 -0400

    create function to retrieve and attach last game results

--- board.js
+++ board.js
@@ -1,20 +1,19 @@
-var bodyEl = document.querySelector("body");
-var ulEl = document.querySelector("ul");
-
-var score = "";
-var user = "";
-
 // Pull recent score stats from local storage
-function init() {
-    score = localStorage.getItem("score");
-    user = localStorage.getItem("user");
+function updateBoard() {
+    // Retrieve local storage values
+    var score = localStorage.getItem("score");
+    var user = localStorage.getItem("user");
 
-    ulEl.append(user);
-    ulEl.append(score);
+    // Insert a new row into the Leader Table
+    var table = document.querySelector("#table-body");
+    var row = document.querySelector("#leaderRow");
+    var firstCell = document.createElement("td");
+    row.appendChild(firstCell);    
+    var secondCell = document.createElement("td");
+    row.appendChild(secondCell);
+    firstCell.textContent = user;
+    secondCell.textContent = score;
 
-
-    console.log(score);
-    console.log(user);
 }
 
-init() 
+updateBoard();
