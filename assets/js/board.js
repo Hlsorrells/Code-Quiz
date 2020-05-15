862286506a78dc619e74ca0f6a11104f7facb437
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-14 21:29:00 -0400

    created dynamic results board from new localStorage object

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,26 +1,24 @@
 var tableEl = document.querySelector(".table");
 var clearBtn = document.querySelector("#clear");
-
+var tableBody = document.querySelector("#scoreArea")
 // Pull recent score stats from local storage
 function updateBoard() {
     // Retrieve local storage values
-    var score = localStorage.getItem("score");
-    var user = localStorage.getItem("user");
+    var highscore = JSON.parse(localStorage.getItem("highscore"));
+    highscore = highscore.sort(function (a, b) { return b.score - a.score });
 
     // Create table body under table header
-    var tbodyEl = document.createElement("tbody");
-    tableEl.appendChild(tbodyEl);
-    var trowEl = document.createElement("tr");
-    tbodyEl.appendChild(trowEl);
-
-    // Insert last game results
-    var firstCell = document.createElement("td");
-    trowEl.appendChild(firstCell);
-    firstCell.textContent = user;
-    var secondCell = document.createElement("td");
-    trowEl.appendChild(secondCell);
-    secondCell.textContent = score;
-
+    for (let i = 0; i < highscore.length; i++) {
+        // Insert last game results
+        var firstCell = document.createElement("td");
+        var trowEl = document.createElement("tr");
+        firstCell.textContent = highscore[i].user;
+        var secondCell = document.createElement("td");
+        secondCell.textContent = highscore[i].score;
+        trowEl.appendChild(firstCell);
+        trowEl.appendChild(secondCell);
+        tableBody.appendChild(trowEl);
+    }
 }
 
 // Update the Leader Board Results
@@ -33,8 +31,7 @@ function resetBoard() {
     localStorage.setItem("user", "");
 
     // Remove all rows from table body
-    var tbodyEl = document.querySelector("tbody");
-    tbodyEl.remove();
+    tableBody.remove();
 
     // Recall updateBoard function to clear cells
     updateBoard();
